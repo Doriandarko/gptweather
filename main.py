da318f1c290de8a9bb8ba70ec8bafc5b7e81be90
Pietro Schirano <pietro.schirano@gmail.com>
2023-04-05 22:52:05 +0000

    Removed API

--- main.py
+++ main.py
@@ -1,57 +1,44 @@
-# Import required modules
 import json
-import os
-from quart import Quart, request, jsonify
-from quart_cors import cors
+import quart_cors
+import quart
+from quart import request, Response
+import requests
 
-# Create a Quart app and enable CORS
-app = Quart(__name__)
-app = cors(app)
+app = quart_cors.cors(quart.Quart(__name__),
+                      allow_origin="https://chat.openai.com")
 
-# Retrieve the service authentication key from the environment variables
-SERVICE_AUTH_KEY = os.environ.get("SERVICE_AUTH_KEY")
-# Initialize an empty dictionary to store todos
-TODOS = {}
 
-# Add a before_request hook to check for authorization header
-@app.before_request
-def auth_required():
-    # Get the authorization header from the request
-    auth_header = request.headers.get("Authorization")
-    # Check if the header is missing or incorrect, and return an error if needed
-    if not auth_header or auth_header != f"Bearer {SERVICE_AUTH_KEY}":
-        return jsonify({"error": "Unauthorized"}), 401
+@app.post("/weather")
+async def get_weather():
+  req_data = await request.get_json(force=True)
+  city = req_data["city"]
+  api_key = "YOUR API KEY HERE"
+  url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+  response = requests.get(url)
+  return Response(response=json.dumps(response.json()), status=200)
 
-# Define a route to get todos for a specific username
-@app.route("/todos/<string:username>", methods=["GET"])
-async def get_todos(username):
-    # Get todos for the given username, or return an empty list if not found
-    todos = TODOS.get(username, [])
-    return jsonify(todos)
 
-# Define a route to add a todo for a specific username
-@app.route("/todos/<string:username>", methods=["POST"])
-async def add_todo(username):
-    # Get the request data as JSON
-    request_data = await request.get_json()
-    # Get the todo from the request data, or use an empty string if not found
-    todo = request_data.get("todo", "")
-    # Add the todo to the todos dictionary
-    TODOS.setdefault(username, []).append(todo)
-    return jsonify({"status": "success"})
+@app.get("/.well-known/ai-plugin.json")
+async def plugin_manifest():
+  host = request.headers['Host']
+  with open("ai-plugin.json") as f:
+    text = f.read()
+    text = text.replace("PLUGIN_HOSTNAME", f"https://{host}")
+    return Response(text, mimetype="text/json")
 
-# Define a route to delete a todo for a specific username
-@app.route("/todos/<string:username>", methods=["DELETE"])
-async def delete_todo(username):
-    # Get the request data as JSON
-    request_data = await request.get_json()
-    # Get the todo index from the request data, or use -1 if not found
-    todo_idx = request_data.get("todo_idx", -1)
-    # Check if the index is valid, and delete the todo if it is
-    if 0 <= todo_idx < len(TODOS.get(username, [])):
-        TODOS[username].pop(todo_idx)
-    return jsonify({"status": "success"})
 
-# Run the app
+@app.get("/openapi.yaml")
+async def openapi_spec():
+  host = request.headers['Host']
+  with open("openapi.yaml") as f:
+    text = f.read()
+    text = text.replace("PLUGIN_HOSTNAME", f"https://{host}")
+    return Response(text, mimetype="text/yaml")
+
+
+def main():
+  app.run(debug=True, host="0.0.0.0", port=5002)
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+  main()
